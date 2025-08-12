47167b3da810c7d7fd6037d87e71e8a032eca1a2
Shiwang Srivastava <r.shiw019@gmail.com>
2025-08-12 10:56:44 +0530

    pwa implemented

--- vite.config.js
+++ vite.config.js
@@ -5,34 +5,34 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'autoUpdate', // Automatically update SW
-    includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'], // Static assets
+    registerType: 'autoUpdate',
+    includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
     manifest: {
-      name: 'My Vite PWA',
-      short_name: 'VitePWA',
-      description: 'A Progressive Web App built with Vite',
-      theme_color: '#42b883',
+      name: 'B2Y Infy Solutions',
+      short_name: 'B2Y',
+      description: 'B2Y Infy Solutions delivers innovative IT solutions to help businesses transform and succeed in the digital landscape.',
+      theme_color: '#ffffff',
       background_color: '#ffffff',
       display: 'standalone',
       start_url: '/',
       icons: [
         {
-          src: '/pwa-192x192.png',
+          src: 'pwa-192x192.png',
           sizes: '192x192',
-          type: 'image/png'
+          type: 'image/png',
         },
         {
-          src: '/pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-        {
-          src: '/pwa-512x512.png',
+          src: 'pwa-512x512.png',
           sizes: '512x512',
           type: 'image/png',
-          purpose: 'any maskable'
-        }
-      ]
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
     }
   })],
 })
