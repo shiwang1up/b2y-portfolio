b599bab7a0d810203f29c2160738a7eef623a923
Shiwang Srivastava <r.shiw019@gmail.com>
2025-08-16 12:21:18 +0530

    feat: implement manual PWA configuration

---
+++ workbox-config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'
+  ],
+  swDest: 'dist/sw.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+    /^fbclid$/
+  ]
+};
